dcf50e2fec5b66da1640639dfb971b18dd7b68f1
alhammanazil <alhammanazil18@gmail.com>
2024-03-02 14:36:59 +0700

    add some features in the game

--- ball-game.py
+++ ball-game.py
@@ -7,11 +7,17 @@ print(welcome_message)
 name = input("🔵 What is your name? ")
 print("👋 Hi, Good to see you, " + name + "!")
 
+box = "|__| "
+boxes = [box] * 4
+
+tmp_boxes = boxes.copy()
+
+boxes[ball_position - 1] = "|0_0|"
+
 print(f'''
 Please take a look at the box below:
-📦  📦  📦  📦
- 1   2   3   4
- ''')
+{' '.join(tmp_boxes)}
+''')
 
 user_choice = int(input("Where do you think the ball is? [1 / 2 / 3 / 4]: "))
 
@@ -20,11 +26,17 @@ if user_choice not in [1, 2, 3, 4]:
     exit()
 
 user_confirmation = input(f"Are you sure you want to choose box number {user_choice}? [Yes / No]: ")
+
 if user_confirmation.lower() == "no":
     print("Please re-enter the box number.")
     user_choice = int(input("Where do you think the ball is? [1 / 2 / 3 / 4]: "))
+elif user_confirmation.lower() != "yes":
+    print("Invalid input. Please input 'Yes' or 'No'.")
+    exit()
 
-if int(user_choice) == ball_position:
+if user_choice == ball_position:
     print(f"🎉 Congratulations, {name}! You found the ball in the box number {ball_position}!")
 else:
-    print(f"Sorry, {name}! The ball was in the box number {ball_position}.")
+    print(f"Sorry, {name}! The ball was in the box number {ball_position}")
+    print(' '.join(boxes))
+    
